6e4ac95b91120dc64085c8e1172cfa6603336b79
ajaymaurya001 <websolutions.ajay@gmail.com>
2025-10-09 18:33:20 +0530

    template setup

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('admin', function () {
+    return view('admin.auth.login');
+});
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
 });
